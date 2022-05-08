18c643ac4a82e03474b678c12be1f12d22514618
Gerardo Vargas-Hernandez <78391890+GerardoVH@users.noreply.github.com>
2022-05-08 00:06:13 -0400

    Implementing API Call to Login

--- JS/signup.js
+++ JS/signup.js
@@ -1,28 +1,30 @@
-
 const signUpUrl = 'http://localhost:2400/api/auth/signup';
 const signInUrl = 'http://localhost:2400/api/auth/login';
 
 function validateSignup(email, password, passwordVerification) {
     if (password !== passwordVerification) {
-        alert('passwords do not match');
-        return false;
+        console.log('passwords do not match');
     } else {
-        let body = {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
             "email": email,
             "password": password
-        }
+        });
 
-        let config = {
-            headers: {
-                'Content-Type': 'application/json;charset=UTF-8',
-                "Access-Control-Allow-Origin": "*",
-            }
-        }
-        axios.post(signUpUrl, body, config)
-            .then(response => { console.log(response) })
-            .catch(error => { console.log(error) })
-        return true;
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+        fetch(signUpUrl, requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
     }
+    return true;
 }
 
 function validateLogin(email, password) {
