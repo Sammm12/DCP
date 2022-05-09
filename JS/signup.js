163fadc7f70a6585f70db692adcfccca0acf342c
Gerardo Vargas-Hernandez <78391890+GerardoVH@users.noreply.github.com>
2022-05-08 21:29:39 -0400

    Finished Login/Signup Logic

--- JS/signup.js
+++ JS/signup.js
@@ -29,5 +29,31 @@ function validateSignup(email, password, passwordVerification) {
 }
 
 function validateLogin(email, password) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    var raw = JSON.stringify({
+        "email": email,
+        "password": password
+    });
 
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("http://localhost:2400/api/auth/login", requestOptions)
+        .then(function (response) {
+            if (response.ok == true) {
+                window.location.href = "http://127.0.0.1:8080/HTML/questionnaire.html";
+            } else if (response.ok == false) {
+                alert('user not found');
+                window.location.href = "http://127.0.0.1:8080/HTML/signup.html";
+            }
+
+            document.getElementById("signInForm").reset()
+        })
+        .catch(error => console.log('error', error));
+    return false;
 }
