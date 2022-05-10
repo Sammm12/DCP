9e23dd85a1a505f30acded3618dfe378f4b51629
Gerardo Vargas-Hernandez <78391890+GerardoVH@users.noreply.github.com>
2022-05-10 17:01:56 -0400

    Adding Classes

---
+++ JS/semesterBoardClassesLogic.js
@@ -0,0 +1,27 @@
+function addClass(course, credits) {
+    var node = createChild(course, credits);
+    var container = document.getElementById("classesHolder");
+    container.appendChild(node);
+}
+
+function createChild(course, credits) {
+    const outterDiv = document.createElement("div");
+    outterDiv.classList.add("row");
+
+    const button = document.createElement("button")
+    button.classList.add('btn', 'btn-danger', 'btn-sm');
+    button.innerHTML = "-";
+    outterDiv.appendChild(button);
+
+    const innerDiv1 = document.createElement("div");
+    innerDiv1.classList.add('col-sm', 'Course', 'text-left');
+    innerDiv1.innerHTML = course;
+
+    const innerDiv2 = document.createElement("div")
+    innerDiv2.classList.add('col-sm', 'ClassCredits');
+    innerDiv2.innerHTML = credits;
+
+    outterDiv.appendChild(innerDiv1);
+    outterDiv.appendChild(innerDiv2);
+    return outterDiv;
+}
